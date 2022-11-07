95277578eb292f458d9b632b695d68d71a109069
Harri Klingsten <powercommand2022@gmail.com>
2022-11-07 13:02:45 +0100

    empty address

--- Templates.BlazorWasmAndAPI/Templates.Shared/DomainObjects/ClientConfiguration.cs
+++ Templates.BlazorWasmAndAPI/Templates.Shared/DomainObjects/ClientConfiguration.cs
@@ -1,5 +1,5 @@
 ﻿namespace Templates.Shared.DomainObjects;
 public class ClientConfiguration
 {
-    public string ProcectApiBaseAddress { get; set; } = "https://localhost:7111/api/";
+    public string ProcectApiBaseAddress { get; set; } = "";
 }
